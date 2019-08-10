ca0ffdae64da819f4d7a0f2b7c9109d5f340ce31
SELVA GANESH SUDALAIYANDI <selva.s.ganesh@gmail.com>
2019-08-09 20:52:37 -0400

    New app changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Selva Ganesh - Vengateshwari - Magizhini (My Daughter) <code>src/App.js</code> and save to reload.
+          My Family - (Selva Ganesh - Vengateshwari - Magizhini) <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
